203a4cdb56c14b016ba1b4f0988c794df4f2e8dd
futuo <bernardomarques5@gmail.com>
2016-04-06 15:40:53 +0100

    service-ctl: [dev] javadoc and code cleaning for the configuration file parsing.

--- jervicectl/src/main/java/com/feedzai/jervicectl/Config.java
+++ jervicectl/src/main/java/com/feedzai/jervicectl/Config.java
@@ -1,6 +1,5 @@
 package com.feedzai.jervicectl;
 
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
@@ -11,23 +10,93 @@ import java.util.regex.Pattern;
 public class Config{
 
     private String filename;
-    private HashMap<String, HashMap> services;
 
     public Config(String filename){
         this.filename = filename;
     }
 
-    private String getFirstMatch(String str, String regex){
-        Matcher matcher = Pattern.compile(regex).matcher(str);
-        matcher.find();
-        return matcher.group();
+    /**
+    * Attempts to parse the configuration file associated with the Config object instance
+    * and returns an object describing the services configuration.
+    *
+    * @return A collection of ServiceData objects describing each service and dependencies*/
+    public boolean parseConfig(){
+        String content = this.getFileContent();
+        return this.parseServices(content);
     }
 
+    /** 
+    * Given a string describing a service, returns the service name
+    *
+    * @param service description as in the configuration file.
+    * @return the service name (i.e. name of its class) */
     private String getServiceName(String serviceDescription){
         String serviceNameRegex = "[^\\{]*";
         return getFirstMatch(serviceDescription, serviceNameRegex).trim();
     }
 
+    /** 
+    * Given a string describing a service, returns the service's dependencies
+    *
+    * @param service description as in the configuration file.
+    * @return the service's dependencies */
+    private ArrayList<String> getServiceDepencies(String serviceDescription){
+        String serviceDepsRegex = "\\{[^\\}]*\\}";
+        String dependencies = getFirstMatch(serviceDescription, serviceDepsRegex).trim();
+
+        return removeEmptyDeps( dependencies.split("[ \\{\\}\\t\\n]+") );
+    }
+
+    /**
+    * Given a string describing a service, returns a ServiceCfg instance
+    * with both the service's class and dependencies list.
+    * 
+    * @param Configuration file text describing the service
+    * @return A ServiceCfg object describing the service configuration*/
+    private ServiceCfg parseService(String service){
+        String name = getServiceName(service);
+        ArrayList<String> dependencies = getServiceDepencies(service);
+        
+        return new ServiceCfg(name, dependencies);
+    }
+
+    /**
+    * @return a string containing all data contained in the file associated with the instance.
+    */
+    private String getFileContent(){
+        String content = "";
+        try{
+            File cfgFile = new File(this.filename);
+            Scanner cfgScanner = new Scanner(cfgFile);
+            content = cfgScanner.useDelimiter("\\Z").next();
+        }catch(FileNotFoundException e){
+            System.err.println("Configuration file " + this.filename + " was not reachable.");
+        }
+        return content;
+    }
+
+    /**
+    * Given the contents of the configuration file returns a collection of ServiceData objects
+    * describing the services and dependencies.
+    *
+    * @return A collection of ServiceData objects describing each service and dependencies*/
+    private ArrayList<ServiceData> parseServices(String content){
+        String serviceEntryRegex = "[^\\{]*\\{[^\\}]*\\}";
+        Pattern pattern = Pattern.compile(serviceEntryRegex);
+        Matcher matcher = pattern.matcher(content);
+        
+        ArrayList<ServiceData> services = ArrayList();
+        while( matcher.find() )
+            services.add( this.parseService( matcher.group() ) );
+        return services;
+    }
+
+    /**
+    * Function to avoid empty string in arrays resulting from string splits.
+    *
+    * @param an array of possible empty strings
+    * @return an ArrayList with all string in deps except empty ones.
+    */
     private ArrayList<String> removeEmptyDeps(String[] deps){
         ArrayList<String> clean = new ArrayList();
         for(String d : deps)
@@ -36,49 +105,18 @@ public class Config{
         return clean;
     }
 
-    private ArrayList<String> getServiceDepencies(String serviceDescription){
-        String serviceDepsRegex = "\\{[^\\}]*\\}";
-        String dependencies = getFirstMatch(serviceDescription, serviceDepsRegex).trim();
-
-        return removeEmptyDeps( dependencies.split("[ \\{\\}\\t\\n]+") );
-    }
-
-    private ServiceWrapper parseService(String service){
-
-        String name = getServiceName(service);
-        ArrayList<String> dependencies = getServiceDepencies(service);
-
-        try{
-            Service s = (Service)Class.forName(name).newInstance();
-            return new ServiceWrapper(s);
-        }catch(ClassNotFoundException | InstantiationException | IllegalAccessException e){
-            e.printStackTrace();
-        }
-
-        return null;
-    }
-
-    public boolean loadConfig(){
-        String content = "";
-        
-        try{
-            File cfgFile = new File(this.filename);
-            Scanner cfgScanner = new Scanner(cfgFile);
-            content = cfgScanner.useDelimiter("\\Z").next();
-        }catch(FileNotFoundException e){
-            e.printStackTrace();
-            return false;
-        }
-
-        String serviceRegex = "[^\\{]*\\{[^\\}]*\\}";
-        Pattern pattern = Pattern.compile(serviceRegex);
-        Matcher matcher = pattern.matcher(content);
-        
-        while(matcher.find()){
-            this.parseService( matcher.group() );
-        }
-
-        return true;
+    /**
+    * Given a string and a regex returns the first match of regex in str.
+    * Useful for situations where we're just there's only one match.
+    *
+    * @param A string to parse
+    * @param A regex to match with the parameter string
+    * @return A string corresponding to the first match of regex in str.
+    */
+    private String getFirstMatch(String str, String regex){
+        Matcher matcher = Pattern.compile(regex).matcher(str);
+        matcher.find();
+        return matcher.group();
     }
 
 }
