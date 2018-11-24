a60d8128cb1bcb10a17d68b55d07c30653c5187c
Bernardo Marques <bernardomarques5@gmail.com>
2018-11-24 16:42:53 +0000

    authenticator: Add unit test running logic

--- authenticator/test.c
+++ authenticator/test.c
@@ -3,9 +3,24 @@
 
 #include "authenticator.h"
 
+typedef struct _test {
+  char *description;
+  int (*test_impl) (void);
+} test;
+
+test tests[] = {
+};
+
 int main(int argc, char** argv) {
 
-  printf("Peido");
+  int count = 0;
+  for (; count < sizeof(tests) / sizeof(test); count++) {
+    test *t = tests + count;
+    printf("Runing test %d: '%s'\n", count + 1, t->description);
+    t->test_impl();
+  }
+
+  printf("finished executing %d tests.\n", count);
 
   return 0;
 }
