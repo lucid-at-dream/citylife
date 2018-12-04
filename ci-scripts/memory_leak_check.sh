8aa10ddef26894ee707ebb64d244a1ec3f8d0d44
Bernardo Marques <bernardomarques5@gmail.com>
2018-12-04 23:14:13 +0000

    continuous integration: Check for memory leaks

---
+++ ci-scripts/memory_leak_check.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+tmpfile=$(mktemp)
+
+valgrind -v $1 2> $tmpfile
+
+cat $tmpfile
+
+leaks=$(echo $(grep lost $tmpfile | cut -d' ' -f2- | grep -ow "[0-9]*") | sed 's/\n//' | sed 's/ //g')
+
+if [ $leaks -gt 0 ]
+then
+  echo "There are memory leaks."
+  exit -1
+fi
+
+exit 0
+
