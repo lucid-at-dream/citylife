d8176660478f08d71ed43d463041c5e3c0eaaf13
Bernardo Marques <bernardomarques5@gmail.com>
2019-03-16 17:05:55 +0000

    authenticator: Bootstrap worker pool

--- authenticator/worker_pool.c
+++ authenticator/worker_pool.c
@@ -1,18 +1,24 @@
+#include "worker_pool.h"
 
-typedef struct _worker_pool {
-    int num_threads;
-    void *(do_work)(void *);
-    int *threads;
-    queue work_queue;
-} worker_pool;
+#include <stdlib.h>
 
-worker_pool *worker_pool_new(int num_threads, void *(do_work)(void *)) {
-    worker_pool pool = (worker_pool *)calloc(1, sizeof(worker_pool));
-    threads = (int *)calloc(num_threads, sizeof(int));
-    pool->num_threads = num_threads;
-    pool->do_work = do_work;
+void pool_add_work(worker_pool *pool, void *job) {
+    queue_add(pool->work_queue, job);
 }
 
-worker_pool_start(worker_pool *pool);
+worker_pool *pool_new(int num_threads, void *(*do_work)(void *)) {
+    worker_pool *pool = (worker_pool *)calloc(1, sizeof(worker_pool));
+    pool->threads = (int *)calloc(num_threads, sizeof(int));
+    pool->num_threads = num_threads;
+    pool->do_work = do_work;
+    pool->work_queue = queue_new();
+    return pool;
+}
 
-worker_pool_stop(worker_pool *pool);
+void pool_start(worker_pool *pool) {
+
+}
+
+void pool_stop(worker_pool *pool) {
+    
+}
--- authenticator/worker_pool.h
+++ authenticator/worker_pool.h
@@ -1,9 +1,14 @@
+#include "queue.h"
 
 typedef struct _worker_pool {
     int num_threads;
-    char *(do_work)(char *);
+    void *(*do_work)(void *);
+    int *threads;
+    queue *work_queue;
 } worker_pool;
 
-worker_pool *worker_pool_new(int num_threads, char *(do_work)(char *));
+worker_pool *worker_pool_new(int num_threads, void *(*)(void *));
 void worker_pool_start(worker_pool *pool);
 void worker_pool_stop(worker_pool *pool);
+
+void pool_add_work(worker_pool *pool, void *job);
